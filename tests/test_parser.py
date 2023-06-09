ba9eee0388179e0f52480bc2925e364f629c32d2
Joshua Rose <joshuarose099@gmail.com>
2023-06-09 23:01:01 +1000

    feat: refactor tests to include `_is_chars`
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,19 +1,17 @@
 import unittest
 import _parser
 
-from _parser import _literals as literals
-
-_literals = [".", ",", "~", "`", "\'", "\"", "\\",
-             "-", "=", "+", "&", "%", "$", "#", "!"]
+from _parser import _literals as literals  # pyright: ignore
 
 class TestParser(unittest.TestCase):
     def test__is_char(self):
         for literal in literals:
-            with self.subTest(isinstance(literal, str)==True):
-                self.assertIn(literal, literals);
+            assert _parser._is_char(literal)
+
+    def test__is_chars(self):
+        for literal in literals:
+            assert _parser._is_chars(literal)
 
     def test_valid_literals(self):
         with self.assertRaises(ValueError):
             _parser.strip_punctuation("a")
-
-
