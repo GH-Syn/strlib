c280e68239bab8719549d1d1ce0dd354ba2317ee
Joshua Rose <joshuarose099@gmail.com>
2023-06-09 12:10:18 +1000

    feat: Add tests for parser

---
+++ tests/test_parser.py
@@ -0,0 +1,18 @@
+import unittest
+import _parser
+
+from _parser import _literals as literals
+
+_literals = [".", ",", "~", "`", "\'", "\"", "\\",
+             "-", "=", "+", "&", "%", "$", "#", "!"]
+
+class TestParser(unittest.TestCase):
+    def test__is_char(self):
+        for literal in literals:
+            self.assertIn(literal, literals);
+
+    def test_valid_literals(self):
+        with self.assertRaises(ValueError):
+            _parser.strip_punctuation("a")
+
+
