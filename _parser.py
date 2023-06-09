b8bdc7e00286eb8cd1fd43d87e76f4f0ebd21343
Joshua Rose <joshuarose099@gmail.com>
2023-06-09 23:33:49 +1000

    style: `PendingDeprecationWarning` decrator tag
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- _parser.py
+++ _parser.py
@@ -1,15 +1,27 @@
-_literals = [".", ",", "~", "`", "\'", "\"", "\\",
+"""
+Author: Joshua Rose <joshuarose099@gmail.com>
+
+String operations and mutability functions for improvement to `re`.
+"""
+
+from typing_extensions import deprecated
+
+
+LITERALS = [".", ",", "~", "`", "\'", "\"", "\\",
              "-", "=", "+", "&", "%", "$", "#", "!"]
+
+__all__ = ['LITERALS']
 
 def _is_char(_char):
     """Assert `char` is valid punctuation character."""
 
-    return _char in _literals
+    return _char in LITERALS
 
+@deprecated("introduced _is_char for character validation")
 def _is_chars(*_chars):
     """Assert all chars are valid punctuation characters."""
 
-    return all(char in _literals for char in _chars)
+    return all(char in LITERALS for char in _chars)
 
 def strip_punctuation(value, *chars):
     """Remove punctuation characters in `value`.
@@ -19,11 +31,15 @@ def strip_punctuation(value, *chars):
      >>> "The quick brown fox jumped over the lazy dog"
     """
 
-    if len(chars) and not _is_chars(chars):
-        raise ValueError("One or more chars aren\'t valid characters")
-    elif not _is_char(value) and not len(chars):
-        raise ValueError("{val} is not a valid punctuation character"
+    _literals = LITERALS.copy()  # *slightly* faster 🚀
+
+    if not _is_char(value) and not len(chars):
+        # NOTE code returns due to `ValueError`
+        raise PendingDeprecationWarning("{val} is not a valid punctuation character"
                          .format(val=value))
+
+    if len(chars) and not _is_chars(chars):
+        _literals.extend(chars)
 
     for literal in _literals:
         if literal in value:
