260044be42ef01a17f8a0059735c47335acf6433
Joshua Rose <joshuarose099@gmail.com>
2023-06-10 22:08:52 +1000

    docs: Add documentation string for `@prototype`
    Thread: #9
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- strlib/_decorators.py
+++ strlib/_decorators.py
@@ -5,6 +5,22 @@ def prototype(function, *args, **kwargs):
     """Mark a function as a work in progress.
 
     :raises NotImplementedError: When called at runtime.
+
+    Usage::
+
+        @deprecated("Use B instead")
+        class A:
+            pass
+
+        @deprecated("Use g instead")
+        def f():
+            pass
+
+        @overload
+        @deprecated("int support is deprecated")
+        def g(x: int) -> int: ...
+        @overload
+        def g(x: str) -> int: ...
     """
 
     def wrapper(*args, **kwargs):
