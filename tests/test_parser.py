841c0b87bc8cc8aa521833869b9b042740995664
Joshua Rose <joshuarose099@gmail.com>
2023-06-10 10:11:31 +1000

    fix: corrects unit test for `parse_url`
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,7 @@
 import unittest
 
 import _parser
-from _parser import LITERALS as literals  # pyright: ignore
+from _parser import LITERALS as literals  # pyright: ignore unknown import
 
 
 class TestParser(unittest.TestCase):
@@ -11,8 +11,19 @@ class TestParser(unittest.TestCase):
 
     def test__is_chars(self):
         for literal in literals:
-            self.subTest(_parser._is_chars(literal))
+            # NOTE deprecation warning is ignored via pyright
+            self.subTest(_parser._is_chars(literal))  # pyright: ignore
 
     def test_valid_literals(self):
         with self.assertRaises(PendingDeprecationWarning):
             _parser.strip_punctuation("a")
+
+    def test_url(self):
+        url_string = _parser.parse_url("https%3A%2F%2Fgoogle%2Ecom")
+
+        assert url_string == "https://google.com"
+
+    def test_url_exclude(self):
+        url_string = _parser.parse_url("https%3A%2F%2Fpython%2Eorg", ".")  # pyright: ignore
+
+        assert url_string == "https://python%2Eorg"
