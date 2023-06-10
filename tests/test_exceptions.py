ae5b433179864fa93829d665f38e857275220ef3
Joshua Rose <joshuarose099@gmail.com>
2023-06-10 14:51:32 +1000

    feat: add exceptions tests
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ tests/test_exceptions.py
@@ -0,0 +1,18 @@
+import unittest
+
+from _exceptions import InvalidCharacter
+
+
+class TestInvalidCharacter(unittest.TestCase):
+    def test_dec_instance(self):
+        # test declare instance of `InvalidCharacter`
+
+        error = InvalidCharacter("test")
+
+        self.assertIsInstance(error, InvalidCharacter)
+
+    def test_raise_instance(self):
+        error = InvalidCharacter("test")
+
+        with self.assertRaises(InvalidCharacter):
+            raise error
