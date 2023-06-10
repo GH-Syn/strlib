650a0d01359d6dedd93cd3b80c86ef51d48ccab7
Joshua Rose <joshuarose099@gmail.com>
2023-06-10 23:22:17 +1000

    docs: Add docstring for `InvalidCharacterError`
    Issue: #9
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- strlib/_exceptions.pyi
+++ strlib/_exceptions.pyi
@@ -7,6 +7,15 @@ Types for _exceptions.py
 from _typeshed import NoneType
 
 class InvalidCharacterError(Exception):
+    """InvalidCharacterError raised when an invalid literal is parsed.
+
+    This class is used to express a mismatched literal when being parsed in
+    a function as either a regular character or as part of a URL.
+
+    Args:
+        char (str): Character literal raised from error
+
+    """
     char: str
 
     def __init__(self, char: str) -> NoneType:
